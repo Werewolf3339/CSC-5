2c228a17287fe9b8dec95f28b2b3c03753bb892f
Werewolf3339 <nobody3339@hotmail.com>
2016-02-24 08:14:25 -0800

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Brandon Fins
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Input values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
